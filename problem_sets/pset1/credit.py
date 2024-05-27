da767ae5b22194bc32fc85b8db4cd1db997a076d
Thiago F. Dias <thiago.fernandesdias235@gmail.com>
2024-05-27 14:13:27 -0300

    Verificação do tamanho da string

--- problem_sets/pset1/credit.py
+++ problem_sets/pset1/credit.py
@@ -1,5 +1,6 @@
-def credit(number: int) -> str:
+def credit(number_string: str) -> str:
     sum = i = 0
+    number = parse_credit_number(number_string)
     number_copy = number
     while number_copy > 0:
         digit = number_copy % 10
@@ -11,7 +12,13 @@ def credit(number: int) -> str:
         number_copy -= digit
         number_copy //= 10
         i += 1
-    if sum % 10 != 0: return "INVALID"
+    if sum % 10 != 0: 
+        return "INVALID"
+    valid_length = False
+    for j in [15, 16, 13]:
+        valid_length = valid_length or number // 10 ** j != 0
+    if not valid_length:
+        return "INVALID"
     card_provider_number = number // 10 ** (i - 2)
     if card_provider_number // 10 == 4:
         return "VISA"
@@ -20,3 +27,11 @@ def credit(number: int) -> str:
     elif card_provider_number in [51, 52, 53, 54, 55]:
         return "MASTERCARD"
     return "OTHER"
+
+def parse_credit_number(number_string: str) -> int:
+    if not number_string.isnumeric():
+        raise ValueError("The number string must contain only the digits!")
+    number_parsed = int(number_string)
+    return number_parsed
+
+
--- problem_sets/pset1/credit_test.py
+++ problem_sets/pset1/credit_test.py
@@ -1,28 +1,36 @@
+import pytest
+
 from credit import credit
 
 def test_visa():
-    assert credit(4003600000000014) == "VISA"
+    assert credit("4003600000000014") == "VISA"
 
 def test_visa1():
-    assert credit(4111111111111111) == "VISA"
+    assert credit("4111111111111111") == "VISA"
 
 def test_visa2():
-    assert credit(4012888888881881) == "VISA"
+    assert credit("4012888888881881") == "VISA"
 
 def test_mastercard():
-    assert credit(5196285159966449) == "MASTERCARD"
+    assert credit("5196285159966449") == "MASTERCARD"
 
 def test_mastercard1():
-    assert credit(5352432590799795) == "MASTERCARD"
+    assert credit("5352432590799795") == "MASTERCARD"
 
 def test_mastercard2():
-    assert credit(5251263802936756) == "MASTERCARD"
-
-def test_invalid():
-    assert credit(6176292929) == "INVALID"
+    assert credit("5251263802936756") == "MASTERCARD"
 
 def test_amex():
-    assert credit(378282246310005) == "AMEX"
+    assert credit("378282246310005") == "AMEX"
 
 def test_amex1():
-    assert credit(378734493671000) == "AMEX"
+    assert credit("378734493671000") == "AMEX"
+
+def test_invalid1():
+    assert credit("6176292929") == "INVALID"
+
+def test_not_numeric_string():
+    with pytest.raises(ValueError):
+        credit("378734-49367-1000")
+    
+
